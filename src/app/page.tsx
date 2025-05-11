a2715d5239687745cd21c1b3fa6445fcbbd03917
Elia <eliapym@hotmail.co.uk>
2025-05-11 17:18:23 +0100

    Created NextJS Template

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">
+        Home
+      </h1>
+      <p className="text-lg">Home page text</p>
+    </main>
+  )
+}
