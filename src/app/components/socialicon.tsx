7e3a0a8ce3eea70ea0eb684dcb8544c8e72f2fd3
Elia <eliapym@hotmail.co.uk>
2025-05-13 22:09:02 +0100

    Added Social Media Icons

---
+++ src/app/components/socialicon.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import "@/app/css/globals.css";
+
+export default function SocialIcon(props: {
+  link: string;
+  variant: "linkedin" | "github" | "email";
+  icon: React.ReactNode;
+  size: number;
+}) {
+  const gradientClass = `${props.variant}-gradient`;
+  
+  return (
+    <div className="w-fit h-fit">
+      <Link href={props.link} target="_blank">
+        <div className={`w-10 h-10 border-2 rounded-lg flex items-center justify-center hover:cursor-pointer transition-all duration-300 ${gradientClass}`}>
+          <div 
+            className="flex items-center justify-center rounded-lg fill-white"
+            style={{ width: `${props.size * 0.25}rem`, height: `${props.size * 0.25}rem` }}
+          >
+            {props.icon}
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import Background from "@/app/components/background";
+import SocialIcon from "@/app/components/socialicon";
+import { LinkedIn, Github, Email } from "@/../public/icons/socialicons";
 
 export default function Home() {
   return (
@@ -9,9 +11,30 @@ export default function Home() {
         <h1 className="text-3xl font-bold">Hi, I&apos;m Elia</h1>
         <hr className="my-6 border-t-2 w-[103%] self-center" />
         <p className="text-5xl">
-          Aspiring <span className="text-cyan-300 font-bold">Software</span> &<br />
+          Aspiring <span className="text-cyan-300 font-bold">Software</span> &
+          <br />
           <span className="text-red-400 font-bold">DevOps</span> Engineer
         </p>
+        <div className="flex flex-row space-x-6">
+          <SocialIcon
+            link="https://www.linkedin.com/in/eliapym/"
+            variant="linkedin"
+            icon={<LinkedIn />}
+            size={6}
+          />
+          <SocialIcon
+            link="https://github.com/EliaPym"
+            variant="github"
+            icon={<Github />}
+            size={7}
+          />
+          <SocialIcon
+            link="mailto:eliapym@hotmail.com"
+            variant="email"
+            icon={<Email />}
+            size={6}
+          />
+        </div>
       </div>
     </Background>
   );
