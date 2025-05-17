4e794e661bd5240c62c528fbbb4cf7c368755d31
Elia <eliapym@hotmail.co.uk>
2025-05-17 01:25:43 +0100

    feat: add InfiniteMovingCards component and utility function for class name merging
    - Implemented InfiniteMovingCards component for creating an infinite scrolling card carousel.
    - Added utility function `cn` for merging class names using clsx and tailwind-merge.

---
+++ src/app/components/cardCarousel.tsx
@@ -0,0 +1,114 @@
+/*
+    Credit: Aceternity - https://aceternity.com (original code - modified by me)
+    Source: https://ui.aceternity.com/components/infinite-moving-cards
+*/
+
+"use client";
+
+import { cn } from "@/lib/utils";
+import React, { useEffect, useState } from "react";
+
+export const InfiniteMovingCards = ({
+  items,
+  direction = "left",
+  speed = "fast",
+  pauseOnHover = true,
+  className,
+}: {
+  items: {
+    icon: React.ReactNode;
+    name: string;
+    colour_bl: string;
+    colour_tr: string;
+  }[];
+  direction?: "left" | "right";
+  speed?: "fast" | "normal" | "slow";
+  pauseOnHover?: boolean;
+  className?: string;
+}) => {
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const scrollerRef = React.useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    addAnimation();
+  }, []);
+  const [start, setStart] = useState(false);
+  function addAnimation() {
+    if (containerRef.current && scrollerRef.current) {
+      const scrollerContent = Array.from(scrollerRef.current.children);
+
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        if (scrollerRef.current) {
+          scrollerRef.current.appendChild(duplicatedItem);
+        }
+      });
+
+      getDirection();
+      getSpeed();
+      setStart(true);
+    }
+  }
+  const getDirection = () => {
+    if (containerRef.current) {
+      if (direction === "left") {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "forwards"
+        );
+      } else {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "reverse"
+        );
+      }
+    }
+  };
+  const getSpeed = () => {
+    if (containerRef.current) {
+      if (speed === "fast") {
+        containerRef.current.style.setProperty("--animation-duration", "20s");
+      } else if (speed === "normal") {
+        containerRef.current.style.setProperty("--animation-duration", "40s");
+      } else {
+        containerRef.current.style.setProperty("--animation-duration", "80s");
+      }
+    }
+  };
+  return (
+    <div
+      ref={containerRef}
+      className={cn(
+        "scroller relative z-20 max-w-7xl overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+        className
+      )}
+    >
+      <ul
+        ref={scrollerRef}
+        className={cn(
+          "flex w-max min-w-full shrink-0 flex-nowrap gap-4 py-4",
+          start && "animate-scroll",
+          pauseOnHover && "hover:[animation-play-state:paused]"
+        )}
+      >
+        {items.map((item, index) => (
+          <li
+            key={index}
+            className="flex flex-col items-center justify-center text-center"
+          >
+            <div
+              className="w-42 h-20 rounded-lg flex items-center justify-center bg-gradient-to-b from-slate-700 to-slate-800"
+            >
+              <div
+                className="w-16 h-16 rounded-lg flex items-center justify-center fill-white"
+                style={{ width: "4rem", height: "4rem" }}
+              >
+                {item.icon}
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
