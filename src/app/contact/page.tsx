21df339934d4a1b395e3ce7fc7a21c543c23c590
Elia <eliapym@hotmail.co.uk>
2025-05-22 09:26:48 +0100

    Added basic theme for input form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,24 @@
+"use client";
+
+import React, { useState } from "react";
 import { Button, TextField } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/material.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 export default function Contact() {
+  const [phone, setPhone] = useState("");
+  const theme = createTheme({
+    colorSchemes: {
+      light: true,
+      dark: true,
+    },
+  });
+
   return (
     <>
-      <div className="relative flex flex-col items-center justify-center w-screen h-full">
+      <div className="flex flex-col items-center justify-center w-screen h-full">
         <h1>Contact</h1>
         {/*
         First Name
@@ -16,65 +30,61 @@ export default function Contact() {
         Submit button
         */}
         <form className="flex flex-col items-center justify-center w-full h-full max-w-md p-4 gap-4">
-          <div className="flex flex-row gap-4 w-full">
+          <ThemeProvider theme={theme}>
+            <div className="flex flex-row gap-4 w-full">
+              <TextField
+                id="first_name"
+                name="first_name"
+                label="First Name"
+                placeholder="John"
+                variant="outlined"
+              />
+              <TextField
+                id="last_name"
+                name="last_name"
+                label="Last Name"
+                placeholder="Doe"
+                variant="outlined"
+              />
+            </div>
             <TextField
-              id="first_name"
-              name="first_name"
-              label="First Name"
-              placeholder="John"
+              id="email"
+              name="email"
+              label="Email"
+              placeholder="johndoe@example.com"
               variant="outlined"
-              value={""}
+              className="w-full"
             />
+            <div className="flex flex-row gap-4 w-full">
+              <PhoneInput
+                value={phone}
+                onChange={(phone) => setPhone(phone)}
+                country="gb"
+                preferredCountries={["gb"]}
+                enableSearch={true}
+              />
+              <TextField
+                id="company"
+                name="company"
+                label="Company (optional)"
+                placeholder="Example Inc."
+                variant="outlined"
+              />
+            </div>
             <TextField
-              id="last_name"
-              name="last_name"
-              label="Last Name"
-              placeholder="Doe"
+              id="message"
+              name="message"
+              label="Message"
+              placeholder="Your message here..."
               variant="outlined"
-              value={""}
+              multiline
+              rows={4}
+              className="w-full"
             />
-          </div>
-          <TextField
-            id="email"
-            name="email"
-            label="Email"
-            placeholder="johndoe@example.com"
-            variant="outlined"
-            value={""}
-            className="w-full"
-          />
-          <div className="flex flex-row gap-4 w-full">
-            <TextField
-              id="phone"
-              name="phone"
-              label="Phone (optional)"
-              placeholder="+44 1234 567890"
-              variant="outlined"
-              value={""}
-            />
-            <TextField
-              id="company"
-              name="company"
-              label="Company (optional)"
-              placeholder="Example Inc."
-              variant="outlined"
-              value={""}
-            />
-          </div>
-          <TextField
-            id="message"
-            name="message"
-            label="Message"
-            placeholder="Your message here..."
-            variant="outlined"
-            multiline
-            rows={4}
-            value={""}
-            className="w-full"
-          />
-          <Button variant="contained" type="submit" endIcon={<SendIcon />}>
-            Submit
-          </Button>
+            <Button variant="contained" type="submit" endIcon={<SendIcon />}>
+              Submit
+            </Button>
+          </ThemeProvider>
         </form>
       </div>
     </>
