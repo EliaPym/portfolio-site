ffb6a7088580aa905fccac017e87f8fa9b891107
Elia <eliapym@hotmail.co.uk>
2025-05-26 19:52:11 +0100

    feat: Added functionality to contact form
    Contact form can now be submitted - will send an email with contents.
    
    Removed dot pattern background as was too CPU intensive - replaced with flickering grid background from home page.
    
    Minor visual and QoL changes to contact form.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,7 +5,8 @@ import { Button, TextField } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { MuiPhone } from "../components/phoneInput";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { DotPattern } from "../components/dotPattern";
+import { FlickeringGrid } from "../components/background";
+import "dotenv/config";
 
 export default function Contact() {
   const [phone, setPhone] = useState("");
@@ -16,21 +17,48 @@ export default function Contact() {
     },
   });
 
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+
+    formData.append(
+      "access_key",
+      process.env.NEXT_PUBLIC_FORM_ACCESS_KEY || ""
+    );
+
+    const object = Object.fromEntries(formData);
+    const jsonData = JSON.stringify(object);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: jsonData,
+    });
+
+    const result = await response.json();
+    if (result.success) {
+      alert("Message sent successfully!");
+      event.currentTarget.reset();
+    } else {
+      alert("Error sending message: " + result.message);
+    }
+  }
+
   return (
     <>
-      <DotPattern
-        width={16}
-        height={16}
-        x={0}
-        y={0}
-        cx={1}
-        cy={1}
-        cr={1}
-        className="absolute inset-0 z-[-1] opacity-50 [mask-image:radial-gradient(ellipse_at_center,white_0%,transparent_77%)]"
-        glow={true}
-      />
-      <div className="flex flex-col items-center justify-center w-screen h-full">
-        <h1>Contact</h1>
+      <div className="flex flex-col items-center justify-center w-full min-h-[calc(100vh-180px)] py-16">
+        <FlickeringGrid
+          className="absolute inset-0 z-[-1] [mask-image:radial-gradient(ellipse_at_center,white_0%,transparent_60%)]"
+          squareSize={5}
+          gridGap={6}
+          color="#60A5FA"
+          maxOpacity={0.5}
+          flickerChance={0.02}
+        />
+        <h1 className="text-4xl font-medium text-primary">Contact</h1>
         {/*
         First Name
         Last Name
@@ -40,7 +68,12 @@ export default function Contact() {
         Message
         Submit button
         */}
-        <form className="flex flex-col items-center justify-center w-full h-full max-w-md p-4 gap-4">
+        <form
+          className="flex flex-col items-center justify-center w-full h-full max-w-md p-4 gap-4"
+          onSubmit={handleSubmit}
+        >
+          <hr className="border-t-1 w-[103%] self-center" />
+
           <ThemeProvider theme={theme}>
             <div className="flex flex-row gap-4 w-full">
               <TextField
@@ -50,6 +83,7 @@ export default function Contact() {
                 placeholder="John"
                 variant="outlined"
                 className="bg-background/80"
+                type="text"
               />
               <TextField
                 id="last_name"
@@ -58,6 +92,7 @@ export default function Contact() {
                 placeholder="Doe"
                 variant="outlined"
                 className="bg-background/80"
+                type="text"
               />
             </div>
             <TextField
@@ -67,12 +102,19 @@ export default function Contact() {
               placeholder="johndoe@example.com"
               variant="outlined"
               className="bg-background/80 w-full"
+              required
+              type="email"
             />
             <div className="flex flex-row gap-4 w-full">
               <MuiPhone
                 value={phone}
                 onChange={(phone) => setPhone(phone)}
                 className="bg-background/80"
+                id="phone"
+                label="Phone number"
+                name="phone"
+                placeholder="+44 01234 567890"
+                type="tel"
               />
               <TextField
                 id="company"
@@ -92,7 +134,11 @@ export default function Contact() {
               multiline
               rows={4}
               className="bg-background/80 w-full"
+              required
+              type="text"
+              slotProps={{ htmlInput: { style: { resize: "block" } } }}
             />
+
             <Button variant="contained" type="submit" endIcon={<SendIcon />}>
               Submit
             </Button>
